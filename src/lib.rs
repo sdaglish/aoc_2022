cc6138bbc9aef619920befd9e88e012d60efffba
Steven Daglish <steven.daglish@etas.com>
2022-11-24 11:24:31 +0000

    Cleaning up reading of lines in read_one_number_per_line

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 type BoxResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,14 +10,7 @@ pub fn read_one_number_per_line(filename: &str) -> BoxResult<Vec<i32>> {
     let mut numbers: Vec<i32> = Vec::new();
 
     for line in reader.lines() {
-        match line?.parse::<i32>() {
-            Ok(l) => {
-                numbers.push(l);
-            },
-            Err(e) => {
-                return Err(Box::new(e))
-            }
-        }
+        numbers.push(line?.parse::<i32>()?);
     }
 
     Ok(numbers)
