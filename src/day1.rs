2f84f457ef6cfd62f41ac55fe5857f887d6545ae
Steven Daglish <steven.daglish@etas.com>
2022-12-01 09:29:19 +0000

    Day1 Part2 done

--- src/day1.rs
+++ src/day1.rs
@@ -1,18 +1,6 @@
 use aoc_2022::*;
 
-// fn calculate_windows_incrementation(numbers: &Vec<i32>, len: usize) -> i32 {
-//     let mut inc = 0;
-//     if len > 1 {
-//         for slice in numbers.windows(len) {
-//             if slice[len - 1] > slice[0] {
-//                 inc += 1;
-//             }
-//         }
-//     }
-//     inc
-// }
-
-fn part1_answer(numbers: &Vec<i32>) -> i32 {
+fn calculate_total_calories_each(numbers: &Vec<i32>) -> Vec<i32> {
     let mut total_calories: Vec<i32> = Vec::new();
     let mut total = 0;
 
@@ -27,19 +15,20 @@ fn part1_answer(numbers: &Vec<i32>) -> i32 {
     }
     total_calories.push(total);
 
-    let mut highest = total_calories[0];
-    for calories in total_calories {
-        if calories > highest {
-            highest = calories;
-        }
-    }
+    total_calories.sort_by(|a, b| b.cmp(a));
 
-    return highest;
+    total_calories
 }
 
-// fn part2_answer(numbers: &Vec<i32>) -> i32 {
-//     calculate_windows_incrementation(numbers, 4)
-// }
+fn part1_answer(numbers: &Vec<i32>) -> i32 {
+    let total_calories = calculate_total_calories_each(numbers);
+    total_calories[0]
+}
+
+fn part2_answer(numbers: &Vec<i32>) -> i32 {
+    let total_calories = calculate_total_calories_each(numbers);
+    total_calories[0] + total_calories[1] + total_calories[2]
+}
 
 fn main() {
     match read_on_number_per_line_with_gaps_to_negative_one("data/day1_data.txt") {
@@ -48,7 +37,7 @@ fn main() {
         }
         Ok(number_vector) => {
             println!("Day 1 part 1 answer: {}", part1_answer(&number_vector));
-            // println!("Day 1 part 2 answer: {}", part2_answer(&number_vector));
+            println!("Day 1 part 2 answer: {}", part2_answer(&number_vector));
         }
     }
 }
@@ -72,14 +61,14 @@ mod test {
         10000];
 
     #[test]
-    fn day1_part1_test() {
+        fn rday1_part1_test() {
         let result = crate::part1_answer(&NUMBER_VECTOR.to_vec());
         assert_eq!(result, 24000);
     }
 
-    // #[test]
-    // fn day1_part2_test() {
-    //     let result = crate::part2_answer(&NUMBER_VECTOR.to_vec());
-    //     assert_eq!(result, 5);
-    // }
+    #[test]
+    fn day1_part2_test() {
+        let result = crate::part2_answer(&NUMBER_VECTOR.to_vec());
+        assert_eq!(result, 45000);
+    }
 }
